7367a616e32b9569c46bc45caba4eec452c0b5f7
Vincenzo Ieva <vincenzoieva@Air-di-Vincenzo.station>
2017-04-12 18:41:38 +0200

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -115,13 +115,13 @@ app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)){
-    return res.satus(404).send();
+    return res.status(404).send();
   }
   Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo){
       return res.status(404).send();
     }
-    res.send(todo);
+    res.send({todo});
   }).catch((e)=>{
     return res.status(400).send();
   });
